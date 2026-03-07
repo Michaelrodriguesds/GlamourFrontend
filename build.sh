68504415d9df6087bfc078b989e14a761c9448d7
Michael Rodrigues <myshelrodrigues@gmail.com>
2026-03-07 11:40:17 -0300

    fix: Flutter 3.27.4 para Dart 3.9

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
+@"
 #!/bin/bash
 set -e
-curl -sL https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.5-stable.tar.xz -o /tmp/flutter.tar.xz
-tar -xf /tmp/flutter.tar.xz -C $HOME
-export PATH="$HOME/flutter/bin:$PATH"
+curl -sL https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.27.4-stable.tar.xz -o /tmp/flutter.tar.xz
+tar -xf /tmp/flutter.tar.xz -C `$HOME
+export PATH="`$HOME/flutter/bin:`$PATH"
 flutter config --enable-web --no-analytics
 flutter pub get
 flutter build web --release
+"@ | Out-File -FilePath "build.sh" -Encoding utf8
