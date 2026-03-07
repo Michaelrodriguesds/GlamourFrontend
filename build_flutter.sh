5cfdbad24e03efdf61bb026ebbe31b1e090d68b6
Michael Rodrigues <myshelrodrigues@gmail.com>
2026-03-07 15:59:50 -0300

    fix: remove apt-get, sem permissão no Netlify

--- build_flutter.sh
+++ build_flutter.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-echo ">>> Instalando dependências do sistema..."
-apt-get update -qq
-apt-get install -y curl git unzip xz-utils zip libglu1-mesa
-
 echo ">>> Baixando Flutter ${FLUTTER_VERSION}..."
 curl -o flutter.tar.xz \
   "https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz"
